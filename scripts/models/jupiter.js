115af4db5a4e39f88eebbc4aedb1101faf1c62db
Carl Furrow <me@carlfurrow.com>
2012-05-19 14:24:51 -0400

    Added jupiter

---
+++ scripts/models/jupiter.js
@@ -0,0 +1,7 @@
+function Jupiter(){
+  this.radius = 71492; // km
+  this.fillStyle = "#FF7100";
+  this.distanceToSunMin = 778000000;
+  this.distanceToSunMax = this.distanceToSunMin; //km
+	this.name="jupiter";
+}
