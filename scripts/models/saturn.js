cb0881a186ca15bcfc44627c41242b2adc8d099a
Carl Furrow <me@carlfurrow.com>
2012-05-19 14:28:04 -0400

    Added saturn

---
+++ scripts/models/saturn.js
@@ -0,0 +1,7 @@
+function Saturn(){
+  this.radius = 12500; // km
+  this.fillStyle = "#FF7100";
+  this.distanceToSunMin = 1400000000;
+  this.distanceToSunMax = this.distanceToSunMin; //km
+	this.name="saturn";
+}
