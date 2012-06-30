a250a708a136612c267bc51d13f900fc97bf180a
Carl Furrow <me@carlfurrow.com>
2012-06-30 14:42:24 -0400

    Added jasmine tests, keyboard controls.

---
+++ js/tests/spec/SolarSpec.js
@@ -0,0 +1,6 @@
+describe("solar",function(){
+	it("has an init method",function(){
+		expect(solar).not.toBe(null);	
+		expect(solar.init).not.toBe(null);
+	});
+});
--- js/viewport.js
+++ js/viewport.js
@@ -1,87 +1,70 @@
 function ViewPort(ctx){
-  this._ctx    = ctx;
-  this._width  = this._ctx.canvas.width;
-  this._height = this._ctx.canvas.height;
-  this._x      = 0;
-  this._y      = 0;
+  this.ctx    = ctx;
+  this.width  = this.ctx.canvas.width;
+  this.height = this.ctx.canvas.height;
+  this.x      = 0;
+  this.y      = 0;
   this._scale  = 1.0;
 }
 
-ViewPort.prototype.setX = function(x){
-  this._x = x;
-};
-
-ViewPort.prototype.getX = function(){
-  return this._x;
-};
-
 ViewPort.prototype.getXMax = function(){
-  return this._x + this._width;
-};
-
-ViewPort.prototype.setY = function(y){
-  this._y = y;
-};
-
-ViewPort.prototype.getY = function(){
-  return this._y;
+  return this.x + this.width;
 };
 
 ViewPort.prototype.getYMax = function(){
-  return this._y + this._height;
+  return this.y + this.height;
 };
 
-ViewPort.prototype.getWidth = function(){
-  return this._width;
+ViewPort.prototype.transX = function(objx){
+  return (objx - this.x) * this.getScale();
 };
 
-ViewPort.prototype.getHeight = function(){
-  return this._height;
+ViewPort.prototype.transY = function(objy){
+  return (objy - this.y) * this.getScale();
 };
 
-ViewPort.prototype.getViewPortX = function(objx){
-  return (objx - this._x) * this.getScale();
-};
-
-ViewPort.prototype.getViewPortY = function(objy){
-  return (objy - this._y) * this.getScale();
+ViewPort.prototype.transToLocal = function(obj){
+  return {
+    x:      this.transX(obj.x),
+    y:      this.transY(obj.y),
+    width:  this.getScale() * obj.width,
+    height: this.getScale() * obj.height,
+    fillStyle: obj.fillStyle
+  };
 };
 
 ViewPort.prototype.setScale = function(newscale){
-  this._scale = newscale;
+  if(newscale !== this._scale){
+    this._scale = newscale;
+    var oldcenter = this.getCenter();
+    this.setCenter(oldcenter[0],oldcenter[1]);
+  }
 };
 
 ViewPort.prototype.getScale = function(){
   return this._scale;
 };
 
-ViewPort.prototype.isObjectVisible = function(obj){
-  var scalex = this.getViewPortX(obj.x);
-  var scaley = this.getViewPortY(obj.y);
-  var scalew = this.getScale() * obj.width;
-  var scaleh = this.getScale() * obj.height;
-  var maxx = (scalex + scalew);
-  var maxy = (scaley + scaleh);
-
-  if( (scalex <= this.getXMax() || scalex >= this.getX()) || (maxx <= this.getXMax() || maxx >= this.getX()) )
-  {
-    if( (scaley <= this.getYMax() || scaley >= this.getY()) || (maxy <= this.getYMax() || maxy >= this.getY()) ){
-      return true;
-    }
-  }
-
-  return false;
+ViewPort.prototype._isObjectHorizontallyVisible = function(obj){
+  return (obj.x >= 0 && obj.x <= this.width) || (obj.x+obj.width >= 0 && obj.x+obj.width <= this.width);
 };
 
-ViewPort.prototype.setCenter = function(x,y){
-  // viewport.x = x-viewport.width
-  this.setX( x - this.getWidth()/2 ); 
-  this.setY( y - this.getHeight()/2 );
+ViewPort.prototype._isObjectVerticallyVisible = function(obj){
+  return (obj.y >= 0 && obj.y <= this.height) || (obj.y+obj.height >= 0 && obj.y+obj.height <= this.height);
+};
+
+ViewPort.prototype.isObjectVisible = function(obj){
+  return this._isObjectHorizontallyVisible(obj) && this._isObjectVerticallyVisible(obj);
+};
+
+ViewPort.prototype.setCenter = function(x1,y1){
+  this.x = ( x1 - this.width/2 ); 
+  this.y = ( y1 - this.height/2 );
 };
 
 ViewPort.prototype.getCenter = function(){
-  var centerx = this.getX()+this.getWidth()/2;
-  var centery = this.getY()+this.getHeight()/2;
+  var centerx = this.x+this.width/2;
+  var centery = this.y+this.height/2;
   return [centerx,centery];
 };
 
